fea46fe56ff664e7b6266d00fc44f9822f1a5c17
Thomas Baier <tbaier@math.bme.hu>
2020-11-11 18:39:25 +0100

    Add angular material

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -6,10 +6,11 @@ import {ScenarioComponent} from './scenario/scenario.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ModalityComponent} from './modality/modality.component';
 import {CommonModule} from "@angular/common";
-import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import {NgxSliderModule} from '@angular-slider/ngx-slider';
 // Testing
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService} from './in-memory-data.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 // import { AppRoutingModule } from './app-routing.module';
 
@@ -18,7 +19,7 @@ import {InMemoryDataService} from './in-memory-data.service';
   declarations: [
     AppComponent,
     ScenarioComponent,
-    ModalityComponent
+    ModalityComponent,
   ],
   imports: [
     CommonModule,
@@ -27,13 +28,15 @@ import {InMemoryDataService} from './in-memory-data.service';
     HttpClientModule,
     // AppRoutingModule,
     NgxSliderModule,
+    BrowserAnimationsModule,
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
-    )
+    ),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
