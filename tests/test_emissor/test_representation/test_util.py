ac85b5236ed8e8de498790068c0f4b1b0a3ea8c7
Thomas Baier <tbaier@math.bme.hu>
2021-05-06 14:43:41 +0200

    Fix serialization of collections   * add cls option also for marshalling to support types not covered by serializer   * add default option to marshalling to support custom serializers   * Add more tests

--- tests/test_emissor/test_representation/test_util.py
+++ tests/test_emissor/test_representation/test_util.py
@@ -1,21 +1,23 @@
+import uuid
 from dataclasses import dataclass
 from typing import Union, Optional
 from unittest import TestCase
+from uuid import UUID
 
 from emissor.representation.util import marshal, unmarshal
 
 
-class TestMarshalling(TestCase):
+class TestMarshallingWithTypes(TestCase):
     def test_plain(self):
         @dataclass
         class TestString:
             label: str
 
         instance = TestString("testString")
-        unmarshalled = unmarshal(marshal(instance), TestString)
+        unmarshalled = unmarshal(marshal(instance, cls=TestString), cls=TestString)
 
         self.assertIsInstance(unmarshalled, TestString)
-        self.assertEquals(unmarshalled.label, "testString")
+        self.assertEqual(unmarshalled.label, "testString")
 
     def test_with_nested(self):
         @dataclass
@@ -27,11 +29,11 @@ class TestMarshalling(TestCase):
             nested: TestString
 
         instance = NestTest(TestString("testString"))
-        unmarshalled = unmarshal(marshal(instance), NestTest)
+        unmarshalled = unmarshal(marshal(instance, cls=NestTest), cls=NestTest)
 
         self.assertIsInstance(unmarshalled, NestTest)
         self.assertIsInstance(unmarshalled.nested, TestString)
-        self.assertEquals(unmarshalled.nested.label, "testString")
+        self.assertEqual(unmarshalled.nested.label, "testString")
 
     def test_with_nested_union(self):
         @dataclass
@@ -47,18 +49,18 @@ class TestMarshalling(TestCase):
             nested: Union[TestString, TestInt]
 
         instance = NestTest(TestString("testString"))
-        unmarshalled = unmarshal(marshal(instance), NestTest)
+        unmarshalled = unmarshal(marshal(instance, cls=NestTest), cls=NestTest)
 
         self.assertIsInstance(unmarshalled, NestTest)
         self.assertIsInstance(unmarshalled.nested, TestString)
-        self.assertEquals(unmarshalled.nested.label, "testString")
+        self.assertEqual(unmarshalled.nested.label, "testString")
 
         instance = NestTest(TestInt(5))
-        unmarshalled = unmarshal(marshal(instance), NestTest)
+        unmarshalled = unmarshal(marshal(instance, cls=NestTest), cls=NestTest)
 
         self.assertIsInstance(unmarshalled, NestTest)
         self.assertIsInstance(unmarshalled.nested, TestInt)
-        self.assertEquals(unmarshalled.nested.id, 5)
+        self.assertEqual(unmarshalled.nested.id, 5)
 
     def test_with_none_value(self):
         @dataclass
@@ -66,36 +68,253 @@ class TestMarshalling(TestCase):
             label: Optional[str]
 
         instance = TestString(None)
-        unmarshalled = unmarshal(marshal(instance), TestString)
+        unmarshalled = unmarshal(marshal(instance, cls=TestString), cls=TestString)
 
         self.assertIsInstance(unmarshalled, TestString)
-        self.assertEquals(unmarshalled.label, None)
+        self.assertEqual(unmarshalled.label, None)
 
     def test_json_with_missing_property(self):
         @dataclass
         class TestString:
             label: Optional[str]
 
-        unmarshalled = unmarshal("{}", TestString)
+        unmarshalled = unmarshal("{}", cls=TestString)
 
         self.assertIsInstance(unmarshalled, TestString)
-        self.assertEquals(unmarshalled.label, None)
+        self.assertEqual(unmarshalled.label, None)
 
-    def test_json_to_namedtuple(self):
+    def test_list(self):
+        @dataclass(frozen=True)
+        class TestString:
+            label: str
+
+        instance = [TestString("test1"), TestString("test2")]
+        unmarshalled = unmarshal(marshal(instance, cls=TestString), cls=TestString)
+
+        self.assertIsInstance(unmarshalled, list)
+        self.assertListEqual(unmarshalled, instance)
+
+    def test_set(self):
+        @dataclass(frozen=True)
+        class TestString:
+            label: str
+
+        instance = {TestString("test1"), TestString("test2")}
+        unmarshalled = unmarshal(marshal(instance, cls=TestString), cls=TestString)
+
+        self.assertIsInstance(unmarshalled, list)
+        self.assertSetEqual(set(unmarshalled), instance)
+
+    def test_type_unsupported_by_serializer(self):
+        @dataclass
+        class TestString:
+            label: uuid.UUID
+
+        instance = TestString(uuid.uuid4())
+        self.assertRaises(TypeError, lambda: marshal(instance, default=vars))
+
+        unmarshalled = unmarshal(marshal(instance, cls=TestString), cls=TestString)
+        self.assertIsInstance(unmarshalled, TestString)
+        self.assertIsInstance(unmarshalled.label, uuid.UUID)
+
+
+class TestMarshallingWithoutTypes(TestCase):
+    def test_plain(self):
+        @dataclass
+        class TestString:
+            label: str
+
+        instance = TestString("testString")
+        unmarshalled = unmarshal(marshal(instance))
+
+        self.assertNotIsInstance(unmarshalled, TestString)
+        self.assertEqual(unmarshalled.label, "testString")
+
+    def test_with_nested(self):
+        @dataclass
+        class TestString:
+            label: str
+
+        @dataclass
+        class NestTest:
+            nested: TestString
+
+        instance = NestTest(TestString("testString"))
+        unmarshalled = unmarshal(marshal(instance))
+
+        self.assertNotIsInstance(unmarshalled, NestTest)
+        self.assertNotIsInstance(unmarshalled.nested, TestString)
+        self.assertEqual(unmarshalled.nested.label, "testString")
+
+    def test_with_nested_union(self):
+        @dataclass
+        class TestString:
+            label: str
+
+        @dataclass
+        class TestInt:
+            id: int
+
+        @dataclass
+        class NestTest:
+            nested: Union[TestString, TestInt]
+
+        instance = NestTest(TestString("testString"))
+        unmarshalled = unmarshal(marshal(instance))
+
+        self.assertNotIsInstance(unmarshalled, NestTest)
+        self.assertNotIsInstance(unmarshalled.nested, TestString)
+        self.assertEqual(unmarshalled.nested.label, "testString")
+
+        instance = NestTest(TestInt(5))
+        unmarshalled = unmarshal(marshal(instance))
+
+        self.assertNotIsInstance(unmarshalled, NestTest)
+        self.assertNotIsInstance(unmarshalled.nested, TestInt)
+        self.assertEqual(unmarshalled.nested.id, 5)
+
+    def test_with_none_value(self):
         @dataclass
         class TestString:
             label: Optional[str]
 
-        unmarshalled = unmarshal('{"label": "testString"}')
+        instance = TestString(None)
+        unmarshalled = unmarshal(marshal(instance))
 
         self.assertNotIsInstance(unmarshalled, TestString)
-        self.assertEquals(unmarshalled.label, "testString")
+        self.assertEqual(unmarshalled.label, None)
 
-    def test_json_with_missing_property_to_namedtuple(self):
+    def test_json_with_missing_property(self):
         @dataclass
         class TestString:
             label: Optional[str]
 
         unmarshalled = unmarshal("{}")
 
+        self.assertNotIsInstance(unmarshalled, TestString)
         self.assertRaises(AttributeError, lambda: unmarshalled.label)
+
+    def test_list(self):
+        @dataclass(frozen=True)
+        class TestString:
+            label: str
+
+        instance = [TestString("test1"), TestString("test2")]
+        json = marshal(instance)
+        unmarshalled = unmarshal(json)
+
+        self.assertIsInstance(unmarshalled, list)
+        self.assertListEqual([u.label for u in unmarshalled], [i.label for i in instance])
+
+    def test_set(self):
+        @dataclass(frozen=True)
+        class TestString:
+            label: str
+
+        instance = {TestString("test1"), TestString("test2")}
+        json = marshal(instance)
+        unmarshalled = unmarshal(json)
+
+        self.assertIsInstance(unmarshalled, list)
+        self.assertSetEqual({u.label for u in unmarshalled}, {i.label for i in instance})
+
+
+class TestOnlyUnmarshallingWithTypes(TestCase):
+    def test_plain(self):
+        @dataclass
+        class TestString:
+            label: str
+
+        instance = TestString("testString")
+        unmarshalled = unmarshal(marshal(instance), cls=TestString)
+
+        self.assertIsInstance(unmarshalled, TestString)
+        self.assertEqual(unmarshalled.label, "testString")
+
+    def test_with_nested(self):
+        @dataclass
+        class TestString:
+            label: str
+
+        @dataclass
+        class NestTest:
+            nested: TestString
+
+        instance = NestTest(TestString("testString"))
+        unmarshalled = unmarshal(marshal(instance), cls=NestTest)
+
+        self.assertIsInstance(unmarshalled, NestTest)
+        self.assertIsInstance(unmarshalled.nested, TestString)
+        self.assertEqual(unmarshalled.nested.label, "testString")
+
+    def test_with_nested_union(self):
+        @dataclass
+        class TestString:
+            label: str
+
+        @dataclass
+        class TestInt:
+            id: int
+
+        @dataclass
+        class NestTest:
+            nested: Union[TestString, TestInt]
+
+        instance = NestTest(TestString("testString"))
+        unmarshalled = unmarshal(marshal(instance), cls=NestTest)
+
+        self.assertIsInstance(unmarshalled, NestTest)
+        self.assertIsInstance(unmarshalled.nested, TestString)
+        self.assertEqual(unmarshalled.nested.label, "testString")
+
+        instance = NestTest(TestInt(5))
+        unmarshalled = unmarshal(marshal(instance), cls=NestTest)
+
+        self.assertIsInstance(unmarshalled, NestTest)
+        self.assertIsInstance(unmarshalled.nested, TestInt)
+        self.assertEqual(unmarshalled.nested.id, 5)
+
+    def test_with_none_value(self):
+        @dataclass
+        class TestString:
+            label: Optional[str]
+
+        instance = TestString(None)
+        unmarshalled = unmarshal(marshal(instance), cls=TestString)
+
+        self.assertIsInstance(unmarshalled, TestString)
+        self.assertEqual(unmarshalled.label, None)
+
+    def test_json_with_missing_property(self):
+        @dataclass
+        class TestString:
+            label: Optional[str]
+
+        unmarshalled = unmarshal("{}", cls=TestString)
+
+        self.assertIsInstance(unmarshalled, TestString)
+        self.assertEqual(unmarshalled.label, None)
+
+    def test_list(self):
+        @dataclass(frozen=True)
+        class TestString:
+            label: str
+
+        instance = [TestString("test1"), TestString("test2")]
+        json = marshal(instance)
+        unmarshalled = unmarshal(json, cls=TestString)
+
+        self.assertIsInstance(unmarshalled, list)
+        self.assertListEqual(unmarshalled, instance)
+
+    def test_set(self):
+        @dataclass(frozen=True)
+        class TestString:
+            label: str
+
+        instance = {TestString("test1"), TestString("test2")}
+        json = marshal(instance)
+        unmarshalled = unmarshal(json, cls=TestString)
+
+        self.assertIsInstance(unmarshalled, list)
+        self.assertSetEqual(set(unmarshalled), instance)
