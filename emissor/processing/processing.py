4a8606928de88f16912714f24af466863a6c3d34
Thomas Baier <tbaier@math.bme.hu>
2021-06-25 09:01:19 +0200

    gh-53 Improve log messages in processing

--- emissor/processing/processing.py
+++ emissor/processing/processing.py
@@ -24,18 +24,23 @@ class DataProcessing:
         self._scenario_initializer = scenario_initializer
         self._signal_processors = signal_processors
 
+    def run(self):
+        self.run_preprocessing()
+        self.run_init()
+        self.run_process()
+
     def run_preprocessing(self):
         for preprocessor in self._preprocessors:
             with preprocessor:
-                logger.info("Preprocessing dataset with %s to %s", preprocessor.__class__.__name__, self._storage.base_path)
+                logger.info("Preprocessing dataset with %s to %s", preprocessor.name, self._storage.base_path)
                 preprocessor.preprocess()
-                logger.info("Finished preprocessing dataset with %s", preprocessor.__class__.__name__)
+                logger.info("Finished preprocessing dataset with %s", preprocessor.name)
 
     def run_init(self):
         if not self._scenario_initializer:
             return
 
-        logger.info("Initialize dataset %s", self._storage.base_path)
+        logger.info("Initialize scenarios %s with %s", self._storage.base_path, self._scenario_initializer.name)
         with self._scenario_initializer:
             for scenario_id in self._storage.list_scenarios():
                 try:
@@ -65,12 +70,8 @@ class DataProcessing:
         for processor in self._signal_processors:
             with processor:
                 for scenario_id in self._storage.list_scenarios():
+                    logger.info("Processing scenario %s with processor %s", scenario_id, processor.name)
                     scenario = self._storage.load_scenario(scenario_id)
                     for modality in scenario.signals:
                         signals = self._storage.load_modality(scenario_id, Modality[modality.upper()])
                         processor.process(scenario, Modality[modality.upper()], signals, self._storage)
-
-    def run(self):
-        self.run_preprocessing()
-        self.run_init()
-        self.run_process()
