d9dc65b47b18c75f5e33a9a617c171f6c31a962d
piekvossen <piekvossen@gmail.com>
2021-07-29 10:38:58 +0200

    Cleaned code and added camera function for image input

--- tests/test_emissor/test_interaction/interactive_scenario_driver.py
+++ tests/test_emissor/test_interaction/interactive_scenario_driver.py
@@ -7,34 +7,6 @@ from emissor.persistence import ScenarioStorage
 from emissor.representation.scenario import Modality, ImageSignal, TextSignal, Mention, Annotation, Scenario
 import tests.test_emissor.test_interaction.driver_util as util
 
-def create_image_signal1(scenario: Scenario, file: str):
-    signal = ImageSignal.for_scenario(scenario.id, datetime.now().microsecond, datetime.now().microsecond, file, [], [])
-    return signal
-
-def create_text_signal1(scenario: Scenario, utterance):
-    signal = TextSignal.for_scenario(scenario.id, datetime.now().microsecond, datetime.now().microsecond, "./text.json", utterance, [])
-    return signal
-
-def create_scenario1(scenarioPath: str, scenarioid: str):
-    myscenariopath = scenarioPath+"/"+scenarioid
-    if not os.path.exists(myscenariopath):
-        os.mkdir(myscenariopath)
-        print("Directory ", myscenariopath, " Created ")
-    else:
-        print("Directory ", myscenariopath, " already exists")
-    imagefolder =  myscenariopath+"/"+"image"
-    if not os.path.exists(imagefolder):
-        os.mkdir(imagefolder)
-        print("Directory ", imagefolder, " Created ")
-    else:
-        print("Directory ", imagefolder, " already exists")
-
-    # so far not needed
-    #textfolder = myscenariopath+"/"+"text"
-
-    scenarioStorage = ScenarioStorage(scenario_path)
-    return scenarioStorage
-
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
