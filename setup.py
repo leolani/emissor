cc4c7f0c9cbbce0eb6c7dee4d39d128f91b85839
Thomas Baier <tbaier@math.bme.hu>
2021-10-27 12:15:48 +0200

    Set versions in setup.py to compatible

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("emissor/representation/README.md", "r") as fh:
 
 setuptools.setup(
     name="emissor",
-    version="0.0.dev3",
+    version="0.0.dev5",
     author="CLTL",
     author_email="piek.vossen@vu.nl",
     description="Representation of multi-modal datasets",
@@ -21,12 +21,12 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['numpy>=1.19',
-                      'marshmallow>=3.11',
-                      'marshmallow-dataclass>=8.4',
-                      'marshmallow-enum>=1.5',
-                      'rdflib>=5.0',
-                      'rdflib-jsonld>=0.5',
-                      'simplejson>=3.17'],
+    install_requires=['numpy~=1.20',
+                      'marshmallow~=3.11',
+                      'marshmallow-dataclass~=8.4',
+                      'marshmallow-enum~=1.5',
+                      'rdflib~=5.0',
+                      'rdflib-jsonld~=0.5',
+                      'simplejson~=3.17'],
     python_requires='>=3.7',
 )
